7fb97c5d9551f6c1b1bad3321e4e71058a9bcfe9
metastasio <myaldzina.m@gmail.com>
2023-11-24 11:06:51 +0300

    Added react-router

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,9 @@
 // import reactLogo from './assets/react.svg';
 // import viteLogo from '/vite.svg';
 
-import Header from '../components/Header';
-import Footer from '../components/Footer'
-import Main from '../components/Main';
+import Header from './components/Header';
+import Footer from './components/Footer'
+import Main from './components/Main';
 import './App.css';
 
 function App() {
