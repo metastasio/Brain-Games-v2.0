220130136badb550ee4211aa7ca1fcdd7d96e98d
metastasio <myaldzina.m@gmail.com>
2023-11-29 14:47:50 +0300

    Added init

--- src/init.jsx
+++ src/init.jsx
@@ -1,9 +1,9 @@
 import { I18nextProvider, initReactI18next } from 'react-i18next';
-import { App } from './App';
+import { App } from './App.jsx';
 import { Provider } from 'react-redux';
 import React from 'react';
 import i18next from 'i18next';
-import store from './store';
+import store from './store/index.js';
 import resources from './services/locales/index.js';
 
 const init = async () => {
