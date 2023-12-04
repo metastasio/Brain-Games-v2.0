26db5351e2d05c64963d27608783ca4a5242e066
metastasio <myaldzina.m@gmail.com>
2023-12-04 14:43:01 +0300

    Applying CSS to cards

--- src/components/GamesMain/Games.jsx
+++ src/components/GamesMain/Games.jsx
@@ -2,21 +2,22 @@ import { useTranslation } from 'react-i18next';
 
 import { getRandomGames } from '../../services/getRandomGames';
 import { GameItem } from '../GameItem/GamItem';
-import './games.css'
+import './games.css';
 
 const randomGames = getRandomGames();
 
 export const Games = () => {
-  // const { signedIn } = useSelector((store) => store.user);
   const { t } = useTranslation();
 
   return (
-    <section className='container'>
+    <section className='games-container'>
       <h2 className='workout'>{t('games.desc')}</h2>
       <p className='reminder'>{t('games.reminder')}</p>
-      {randomGames.map((game, i) => (
-        <GameItem game={game} key={i} />
-      ))}
+      <div className='cards-container'>
+        {randomGames.map((game, i) => (
+          <GameItem game={game} key={i} />
+        ))}
+      </div>
     </section>
   );
 };
